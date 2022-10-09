597a54040eda80dad1af3f064082cf41a80d19cf
Hu <734953453@qq.com>
2022-10-09 19:28:30 +0800

    fix: bug

--- src/main/java/com/chuncongcong/health/controller/ReportController.java
+++ src/main/java/com/chuncongcong/health/controller/ReportController.java
@@ -49,78 +49,82 @@ import io.swagger.annotations.ApiOperation;
 @Api(value = "report", tags = "每日报表")
 public class ReportController {
 
-	@Autowired
-	private IVSmStatService ivSmStatService;
+    @Autowired
+    private IVSmStatService ivSmStatService;
 
-	@Autowired
-	private ITSmStatService itSmStatService;
+    @Autowired
+    private ITSmStatService itSmStatService;
 
-	@GetMapping("/health")
-	@ApiOperation("根据设备号和日期查询报表")
-	public HealthReportVo getReport(HealthReportQueryVo healthReportQueryVo) {
-		if(StrUtil.isEmpty(healthReportQueryVo.getDeviceCode()) || StrUtil.isEmpty(healthReportQueryVo.getStartTime())) {
-			throw new ServiceException("参数异常");
-		}
+    @GetMapping("/health")
+    @ApiOperation("根据设备号和日期查询报表")
+    public HealthReportVo getReport(HealthReportQueryVo healthReportQueryVo) {
+        if (StrUtil.isEmpty(healthReportQueryVo.getDeviceCode())
+            || StrUtil.isEmpty(healthReportQueryVo.getStartTime())) {
+            throw new ServiceException("参数异常");
+        }
 
-		HealthReportVo healthReportVo = new HealthReportVo();
-		LocalDate startTime = LocalDate.parse(healthReportQueryVo.getStartTime(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-		LocalDate endTime = LocalDate.parse(healthReportQueryVo.getEndTime(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        HealthReportVo healthReportVo = new HealthReportVo();
+        LocalDate startTime =
+            LocalDate.parse(healthReportQueryVo.getStartTime(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate endTime =
+            LocalDate.parse(healthReportQueryVo.getEndTime(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-		LambdaQueryWrapper<VSmStatPo> vSmStatPoQuery = Wrappers.lambdaQuery(VSmStatPo.class);
-		vSmStatPoQuery.eq(VSmStatPo::getDeviceCode, healthReportQueryVo.getDeviceCode());
-		vSmStatPoQuery.eq(VSmStatPo::getFlagDate, startTime);
-		VSmStatPo vSmStatPo = ivSmStatService.getOne(vSmStatPoQuery);
-		if(Objects.nonNull(vSmStatPo)) {
-			String pgSleepTime = HttpRequest.post("https://hasura.d.leyinlin.com/v1/graphql")
-					.header(Header.CONTENT_TYPE, "application/json")
-					.header("x-hasura-admin-secret", "myadminsecretkey")
-					.body("{\"query\":\"query {\\r\\n  iot_report(where: {serial_no: {_eq: " + healthReportQueryVo.getDeviceCode() + "}, _and: {dt: {_eq: " + startTime.format(DateTimeFormatter.ofPattern("yyyyMMdd")) + "}}}) {\\r\\n    create_time\\r\\n    bed_time\\r\\n  }\\r\\n}\\r\\n\",\"variables\":{}}")
-					.execute().body();
+        LambdaQueryWrapper<VSmStatPo> vSmStatPoQuery = Wrappers.lambdaQuery(VSmStatPo.class);
+        vSmStatPoQuery.eq(VSmStatPo::getDeviceCode, healthReportQueryVo.getDeviceCode());
 
-			JSONObject jsonObject = JSONUtil.parseObj(pgSleepTime).getJSONObject("data").getJSONObject("iot_report");
-			String upTime = jsonObject.getStr("up_time");
-			String bedTime = jsonObject.getStr("bed_time");
+        vSmStatPoQuery.eq(VSmStatPo::getFlagDate, startTime);
+        VSmStatPo vSmStatPo = ivSmStatService.getOne(vSmStatPoQuery);
+        if (Objects.nonNull(vSmStatPo)) {
+            String query = "{\"query\":\"query {\\r\\n  iot_report(where: {serial_no: {_eq: \\\""+ healthReportQueryVo.getDeviceCode() +"\\\"}, _and: {dt: {_eq: "+ startTime.format(DateTimeFormatter.ofPattern("yyyyMMdd")) +"}}}) {\\r\\n    create_time\\r\\n    bed_time\\r\\n  }\\r\\n}\\r\\n\",\"variables\":{}}";
+            String pgSleepTime = HttpRequest.post("https://hasura.d.leyinlin.com/v1/graphql")
+                .header(Header.CONTENT_TYPE, "application/json").header("x-hasura-admin-secret", "myadminsecretkey")
+                .body(query).execute().body();
+			try {
+				JSONObject jsonObject = JSONUtil.parseObj(pgSleepTime).getJSONObject("data").getJSONObject("iot_report");
+				String upTime = jsonObject.getStr("up_time");
+				String bedTime = jsonObject.getStr("bed_time");
+				if (StrUtil.isNotBlank(upTime)) {
+					vSmStatPo.setSleepStart(LocalDateTimeUtil.parse(upTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+				}
+				if (StrUtil.isNotBlank(bedTime)) {
+					vSmStatPo.setSleepEnd(LocalDateTimeUtil.parse(bedTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+				}
+			} catch (Exception e) {
 
-			if(StrUtil.isNotBlank(upTime)) {
-				vSmStatPo.setSleepStart(LocalDateTimeUtil.parse(upTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
 			}
-			if(StrUtil.isNotBlank(bedTime)) {
-				vSmStatPo.setSleepEnd(LocalDateTimeUtil.parse(bedTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-			}
-			healthReportVo.setSleepInfoVo(BeanUtil.copyProperties(vSmStatPo, SleepInfoVo.class));
-		}
+            healthReportVo.setSleepInfoVo(BeanUtil.copyProperties(vSmStatPo, SleepInfoVo.class));
+        }
 
-		LambdaQueryWrapper<TSmStatPo> tSmStatPoQuery = Wrappers.lambdaQuery(TSmStatPo.class);
-		tSmStatPoQuery.eq(TSmStatPo::getDeviceCode, healthReportQueryVo.getDeviceCode());
-		tSmStatPoQuery.ge(TSmStatPo::getStatTime, startTime.atStartOfDay());
-		tSmStatPoQuery.le(TSmStatPo::getStatTime, LocalDateTime.of(endTime, LocalTime.MAX));
-		List<TSmStatPo> tSmStatPos = itSmStatService.list(tSmStatPoQuery);
-		if(CollectionUtil.isNotEmpty(tSmStatPos)) {
-			List<SleepStatusVo> sleepStatusVos = new ArrayList<>();
-			List<HeartRateVo> heartRateVos = new ArrayList<>();
-			List<RespiratoryRateVo> respiratoryRateVos = new ArrayList<>();
-			for (TSmStatPo tSmStatPo : tSmStatPos) {
-				SleepStatusVo sleepStatusVo = new SleepStatusVo();
-				sleepStatusVo.setStatus(tSmStatPo.getStat2());
-				sleepStatusVo.setTime(tSmStatPo.getStatTime());
-				sleepStatusVos.add(sleepStatusVo);
+        LambdaQueryWrapper<TSmStatPo> tSmStatPoQuery = Wrappers.lambdaQuery(TSmStatPo.class);
+        tSmStatPoQuery.eq(TSmStatPo::getDeviceCode, healthReportQueryVo.getDeviceCode());
+        tSmStatPoQuery.ge(TSmStatPo::getStatTime, startTime.atStartOfDay());
+        tSmStatPoQuery.le(TSmStatPo::getStatTime, LocalDateTime.of(endTime, LocalTime.MAX));
+        List<TSmStatPo> tSmStatPos = itSmStatService.list(tSmStatPoQuery);
+        if (CollectionUtil.isNotEmpty(tSmStatPos)) {
+            List<SleepStatusVo> sleepStatusVos = new ArrayList<>();
+            List<HeartRateVo> heartRateVos = new ArrayList<>();
+            List<RespiratoryRateVo> respiratoryRateVos = new ArrayList<>();
+            for (TSmStatPo tSmStatPo : tSmStatPos) {
+                SleepStatusVo sleepStatusVo = new SleepStatusVo();
+                sleepStatusVo.setStatus(tSmStatPo.getStat2());
+                sleepStatusVo.setTime(tSmStatPo.getStatTime());
+                sleepStatusVos.add(sleepStatusVo);
 
-				HeartRateVo heartRateVo = new HeartRateVo();
-				heartRateVo.setNums(tSmStatPo.getHRate());
-				heartRateVo.setTime(tSmStatPo.getStatTime());
-				heartRateVos.add(heartRateVo);
+                HeartRateVo heartRateVo = new HeartRateVo();
+                heartRateVo.setNums(tSmStatPo.getHRate());
+                heartRateVo.setTime(tSmStatPo.getStatTime());
+                heartRateVos.add(heartRateVo);
 
-				RespiratoryRateVo respiratoryRateVo = new RespiratoryRateVo();
-				respiratoryRateVo.setNums(tSmStatPo.getRespRate());
-				respiratoryRateVo.setTime(tSmStatPo.getStatTime());
-				respiratoryRateVos.add(respiratoryRateVo);
-			}
-			healthReportVo.setSleepStatusVos(sleepStatusVos);
-			healthReportVo.setHeartRateVos(heartRateVos);
-			healthReportVo.setRespiratoryRateVos(respiratoryRateVos);
-		}
-		return healthReportVo;
-	}
-
+                RespiratoryRateVo respiratoryRateVo = new RespiratoryRateVo();
+                respiratoryRateVo.setNums(tSmStatPo.getRespRate());
+                respiratoryRateVo.setTime(tSmStatPo.getStatTime());
+                respiratoryRateVos.add(respiratoryRateVo);
+            }
+            healthReportVo.setSleepStatusVos(sleepStatusVos);
+            healthReportVo.setHeartRateVos(heartRateVos);
+            healthReportVo.setRespiratoryRateVos(respiratoryRateVos);
+        }
+        return healthReportVo;
+    }
 
 }
